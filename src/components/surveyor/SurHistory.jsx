38596a02fa3a02a63d22be00b60b7bf4a7668704
SuraZz007 <surajdev9886@gmail.com>
2024-02-01 13:15:10 +0530

    test

--- src/components/surveyor/SurHistory.jsx
+++ src/components/surveyor/SurHistory.jsx
@@ -1,59 +1,11 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import "./style.css";
 import MobHeader from "../header/MobHeader";
 import MobileModal from "../menu/MobileModal";
 import { useMobHeaderContext } from "../../context/MobHeader";
-import axios, { all } from "axios";
-import "./style.css";
-import SurHistoryModal from "./SurHistoryModal";
 
 const SurHistory = () => {
   const { isMobModalOpen, closeMobModal } = useMobHeaderContext();
-  const [responseData, setResponseData] = useState([]);
-
-  // useEffect(() => {
-  //   axios
-  //     .get(
-  //       "http://13.235.80.103:5200/com/getBoothAddress?assembly=Baramati&taluka=199 Daund&booth=10 - Pilanavadi",
-  //       headerConfig
-  //     )
-  //     .then((res) => {
-  //       setResponseData(res.data.ward);
-  //       console.log(res);
-  //     })
-  //     .catch((error) => {
-  //       console.log(error.res);
-  //     });
-  // }, []);
-  const token = localStorage.getItem("accessToken");
-
-  const fetchAssembly = async () => {
-    var myHeaders = new Headers();
-    myHeaders.append("Authorization", `Bearer ${token}`);
-
-    var requestOptions = {
-      method: "GET",
-      headers: myHeaders,
-      redirect: "follow",
-    };
-    try {
-      const response = await fetch(
-        "http://13.235.80.103:5200/auth/getVolunteersDataBySurveyorId?surveyor_id=2",
-        requestOptions
-      );
-      const data = await response.json();
-      // console.log(data);
-      const allData = data.data;
-      setResponseData(allData);
-      console.log(allData);
-    } catch (error) {
-      console.log("error", error);
-    }
-  };
-
-  useEffect(() => {
-    fetchAssembly();
-  }, []);
-
   return (
     <>
       <div className="pg__Wrap">
@@ -67,7 +19,7 @@ const SurHistory = () => {
                     <i className="fa-solid fa-magnifying-glass"></i>
                   </span>
                   <input
-                    type="search"
+                    type="date"
                     className="form-control"
                     list="datalistOptions"
                     id="exampleDataList"
@@ -90,7 +42,38 @@ const SurHistory = () => {
                     aria-label="Text input with dropdown button"
                   />
                   <button
-                    className="btn btn-outline-secondary dropdown-toggle"
+                    class="btn btn-outline-secondary dropdown-toggle"
+                    type="button"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Parliament
+                  </button>
+                  <ul className="dropdown-menu dropdown-menu-end">
+                    <li>
+                      <a className="dropdown-item">Parliament1</a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item">Parliament2</a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item">Parliament3</a>
+                    </li>
+                    <li>
+                      <a className="dropdown-item">Parliament4</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="sur_as">
+                <div className="input-group">
+                  <input
+                    type="text"
+                    className="form-control"
+                    aria-label="Text input with dropdown button"
+                  />
+                  <button
+                    class="btn btn-outline-secondary dropdown-toggle"
                     type="button"
                     data-bs-toggle="dropdown"
                     aria-expanded="false"
@@ -113,39 +96,8 @@ const SurHistory = () => {
                   </ul>
                 </div>
               </div>
-              <div className="sur_as">
-                <div className="input-group">
-                  <input
-                    type="text"
-                    className="form-control"
-                    aria-label="Text input with dropdown button"
-                  />
-                  <button
-                    className="btn btn-outline-secondary dropdown-toggle"
-                    type="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Taluka
-                  </button>
-                  <ul className="dropdown-menu dropdown-menu-end">
-                    <li>
-                      <a className="dropdown-item">Taluka1</a>
-                    </li>
-                    <li>
-                      <a className="dropdown-item">Taluka2</a>
-                    </li>
-                    <li>
-                      <a className="dropdown-item">Taluka3</a>
-                    </li>
-                    <li>
-                      <a className="dropdown-item">Taluka4</a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
             </div>
-            {/* <div className="sur_Row3">
+            <div className="sur_Row3">
               <div className="sur_bt">
                 <div className="input-group">
                   <input
@@ -154,7 +106,7 @@ const SurHistory = () => {
                     aria-label="Text input with dropdown button"
                   />
                   <button
-                    className="btn btn-outline-secondary dropdown-toggle"
+                    class="btn btn-outline-secondary dropdown-toggle"
                     type="button"
                     data-bs-toggle="dropdown"
                     aria-expanded="false"
@@ -177,7 +129,7 @@ const SurHistory = () => {
                   </ul>
                 </div>
               </div>
-            </div> */}
+            </div>
             {/* <div className='sur_Row4'>
               <div className='sur_add-vl'>
                 <button className='btn btn-primary' data-bs-toggle="modal" data-bs-target="#addVolModal">
@@ -197,27 +149,81 @@ const SurHistory = () => {
                 </tr>
               </thead>
               <tbody className="">
-                {responseData.map((data, i) => (
-                  <tr key={i} className="">
-                    <td>{data.assembly}</td>
-                    <td>{data.taluka}</td>
-                    <td>{data.booth}</td>
-                    <td className="">
-                      <button
-                        type="button"
-                        className="btn"
-                        data-bs-toggle="modal"
-                        data-bs-target={`#surEditModal-${i}`} // Add unique identifier for each modal
-                      >
-                        <i className="fa-solid fa-eye"></i>
-                      </button>
-                    </td>
-                    <SurHistoryModal
-                      data={data}
-                      modalId={`surEditModal-${i}`}
-                    />
-                  </tr>
-                ))}
+                <tr className="">
+                  <td>Assembly</td>
+                  <td>Thaluka</td>
+                  <td>Booth</td>
+                  <td className="">
+                    <button
+                      type="button"
+                      class="btn"
+                      data-bs-toggle="modal"
+                      data-bs-target="#surEditModal"
+                    >
+                      <i className="fa-solid fa-eye"></i>
+                    </button>
+                  </td>
+                </tr>
+                <tr className="">
+                  <td>Assembly</td>
+                  <td>Thaluka</td>
+                  <td>Booth</td>
+                  <td className="">
+                    <button
+                      type="button"
+                      class="btn"
+                      data-bs-toggle="modal"
+                      data-bs-target="#surEditModal"
+                    >
+                      <i className="fa-solid fa-eye"></i>
+                    </button>
+                  </td>
+                </tr>
+                <tr className="">
+                  <td>Assembly</td>
+                  <td>Thaluka</td>
+                  <td>Booth</td>
+                  <td className="">
+                    <button
+                      type="button"
+                      class="btn"
+                      data-bs-toggle="modal"
+                      data-bs-target="#surEditModal"
+                    >
+                      <i className="fa-solid fa-eye"></i>
+                    </button>
+                  </td>
+                </tr>
+                <tr className="">
+                  <td>Assembly</td>
+                  <td>Thaluka</td>
+                  <td>Booth</td>
+                  <td className="">
+                    <button
+                      type="button"
+                      class="btn"
+                      data-bs-toggle="modal"
+                      data-bs-target="#surEditModal"
+                    >
+                      <i className="fa-solid fa-eye"></i>
+                    </button>
+                  </td>
+                </tr>
+                <tr className="">
+                  <td>Assembly</td>
+                  <td>Thaluka</td>
+                  <td>Booth</td>
+                  <td className="">
+                    <button
+                      type="button"
+                      class="btn"
+                      data-bs-toggle="modal"
+                      data-bs-target="#surEditModal"
+                    >
+                      <i className="fa-solid fa-eye"></i>
+                    </button>
+                  </td>
+                </tr>
               </tbody>
             </table>
           </div>
@@ -227,6 +233,206 @@ const SurHistory = () => {
         isOpen={isMobModalOpen}
         onClose={closeMobModal}
       ></MobileModal>
+      <div
+        class="modal fade sur_edit-modal"
+        id="surEditModal"
+        tabindex="-1"
+        aria-labelledby="surEditModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <div class="modal-header">
+              {/* <h1 class="modal-title fs-5" id="surEditModalLabel">Modal title</h1> */}
+              {/* <span className='ad_si-no'>01</span> */}
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">
+              <div className="sur_m-cnt">
+                <div className="sur_input-box">
+                  <span className="sr_ed-inp-n">Parliament</span>
+                  <div className="sr_ed-inp-fld">
+                    <select
+                      class="form-select"
+                      aria-label="Default select example"
+                    >
+                      <option selected>Select Parliament</option>
+                      <option value="1">One</option>
+                      <option value="2">Two</option>
+                      <option value="3">Three</option>
+                    </select>
+                  </div>
+                </div>
+                <div className="sur_input-box">
+                  <span className="sr_ed-inp-n">Assembly</span>
+                  <div className="sr_ed-inp-fld">
+                    <select
+                      class="form-select"
+                      aria-label="Default select example"
+                    >
+                      <option selected>Select Assembly</option>
+                      <option value="1">One</option>
+                      <option value="2">Two</option>
+                      <option value="3">Three</option>
+                    </select>
+                  </div>
+                </div>
+                <div className="sur_input-box">
+                  <span className="sr_ed-inp-n">Booth</span>
+                  <div className="sr_ed-inp-fld">
+                    <select
+                      class="form-select"
+                      aria-label="Default select example"
+                    >
+                      <option selected>Select Booth</option>
+                      <option value="1">One</option>
+                      <option value="2">Two</option>
+                      <option value="3">Three</option>
+                    </select>
+                  </div>
+                </div>
+                <div className="sur_input-box">
+                  <span className="sr_ed-inp-n">Booth Address</span>
+                  <div className="sr_ed-inp-fld">
+                    <input
+                      type="text"
+                      class="form-control"
+                      id=""
+                      aria-describedby=""
+                    ></input>
+                  </div>
+                </div>
+                <div className="sur_input-box">
+                  <span className="sr_ed-inp-n">Volunteer Name</span>
+                  <div className="sr_ed-inp-fld">
+                    <input
+                      type="text"
+                      class="form-control"
+                      id=""
+                      aria-describedby=""
+                    ></input>
+                  </div>
+                </div>
+                <div className="sur_input-box">
+                  <span className="sr_ed-inp-n">Phone No</span>
+                  <div className="sr_ed-inp-fld">
+                    <input
+                      type="tel"
+                      class="form-control"
+                      id=""
+                      aria-describedby=""
+                    ></input>
+                  </div>
+                </div>
+                <div className="sur_input-box">
+                  <span className="sr_ed-inp-n">Email ID</span>
+                  <div className="sr_ed-inp-fld">
+                    <input
+                      type="email"
+                      class="form-control"
+                      id=""
+                      aria-describedby=""
+                    ></input>
+                  </div>
+                </div>
+                <div className="sur_input-box">
+                  <span className="sr_ed-inp-n">Gender</span>
+                  <div className="sr_ed-inp-fld">
+                    <select
+                      class="form-select"
+                      aria-label="Default select example"
+                    >
+                      <option selected>Male</option>
+                      <option value="1">Female</option>
+                      <option value="2">Others</option>
+                    </select>
+                  </div>
+                </div>
+                <div className="sur_input-box">
+                  <span className="sr_ed-inp-n">House no</span>
+                  <div className="sr_ed-inp-fld">
+                    <input
+                      type="text"
+                      class="form-control"
+                      id=""
+                      aria-describedby=""
+                    ></input>
+                  </div>
+                </div>
+                <div className="sur_input-box">
+                  <span className="sr_ed-inp-n">Age</span>
+                  <div className="sr_ed-inp-fld">
+                    <input
+                      type="text"
+                      class="form-control"
+                      id=""
+                      aria-describedby=""
+                    ></input>
+                  </div>
+                </div>
+                <div className="sur_input-box">
+                  <span className="sr_ed-inp-n">Caste</span>
+                  <div className="sr_ed-inp-fld">
+                    <input
+                      type="text"
+                      class="form-control"
+                      id=""
+                      aria-describedby=""
+                    ></input>
+                  </div>
+                </div>
+                <div className="sur_input-box">
+                  <span className="sr_ed-inp-n">Occupation</span>
+                  <div className="sr_ed-inp-fld">
+                    <input
+                      type="text"
+                      class="form-control"
+                      id=""
+                      aria-describedby=""
+                    ></input>
+                  </div>
+                </div>
+                <div className="sur_input-box">
+                  <span className="sr_ed-inp-n">Degination</span>
+                  <div className="sr_ed-inp-fld">
+                    <select
+                      class="form-select"
+                      aria-label="Default select example"
+                    >
+                      <option selected>Select Role</option>
+                      <option value="1">One</option>
+                      <option value="2">Two</option>
+                      <option value="3">Three</option>
+                    </select>
+                  </div>
+                </div>
+                <div className="sur_vl-btn">
+                  <button
+                    type="button"
+                    class="btn btn-danger"
+                    data-bs-dismiss="modal"
+                  >
+                    Delete
+                  </button>
+                  &nbsp;&nbsp;&nbsp;
+                  <button type="button" class="btn btn-success">
+                    Edit
+                  </button>
+                </div>
+              </div>
+            </div>
+            {/* <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary">Save changes</button>
+                        </div> */}
+          </div>
+        </div>
+      </div>
     </>
   );
 };
